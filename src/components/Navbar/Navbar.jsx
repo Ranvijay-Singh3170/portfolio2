4299b0c053004cbe2260fdbc477c4b0bf33e3938
Ranvijay-Singh3170 <rvsyadav9793@gmail.com>
2025-07-25 11:18:14 +0530

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { useState } from "react";
+
+const Navbar = () => {
+    const [activeSection, setActiveSection] = useState();
+    const handleMenuItemClick=(sectionId)
+  const menuItems = [
+    { id: "about", label: "About" },
+    { id: "skills", label: "skills" },
+    { id: "experience", label: "Experience" },
+    { id: "work", label: "Work" },
+
+    { id: "education", label: "Education" },
+  ];
+
+  return (
+    <nav className="bg-transparent">
+      <div className="text-white py-5 flex justify-between items-center">
+        <div className="text-lg font-semibold cursor-pointer">
+          <span className="text-[#8245ec]">&lt;</span>
+          <span className="text-white">Ranvijay</span>
+          <span className="text-[#8245ec]">/</span>
+          <span className="text-white">Manish</span>
+          <span className="text-[#8245ec]">&gt;</span>
+        </div>
+        {/* Desktop Menu */}
+        <ul className="md:flex space-x-8 text-gray-300">
+          {menuItems.map((item) => (
+            <li key={item.id } className={`cursor-pointer hover:text-[#8245ec] ${activeSection===item.id ? "text-[#8245ec]" : "text-gray-300"}`}>
+              <button onClick={()=>handleMenuItemClick(item.id)}>{item.label}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
