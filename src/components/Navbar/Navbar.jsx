2f5a06a159fcde76837347df8bbcc04cfe07e174
Ranvijay-Singh3170 <rvsyadav9793@gmail.com>
2025-07-25 17:41:08 +0530

    change css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
             <FaGithub size={24} />
           </a>
           <a
-            href="https://www.linkedin.com/in/tarun-kaushik-553b441a4"
+            href="https://www.linkedin.com/in/ranvijay-singh-b11687249/"
             target="_blank"
             rel="noopener noreferrer"
             className="text-gray-300 hover:text-[#8245ec]"
@@ -122,7 +122,7 @@ const Navbar = () => {
             {/* Social Icons in Mobile */}
             <li className="flex space-x-4 pt-2">
               <a
-                href="https://github.com/codingmastr"
+                href="https://github.com/Ranvijay-Singh3170"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-gray-300 hover:text-white"
